38e88b324078f2c4ee690974949ec6602e7c5711
Raj Marde <rajmarde615@gmail.com>
2023-10-22 01:26:27 +0530

    static code modified

--- server.js
+++ server.js
@@ -7,36 +7,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 require("./db conn/connection");
 
+// Define the path to your frontend views folder
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static assets (e.g., CSS and JavaScript) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// // Set EJS as the view engine
-// app.set("view engine", "ejs");
-
-// Define the path to your frontend views folder
-app.set("views", path.join(__dirname, "backend", "FrontEnd", "views"));
-
-// // Serve static assets (e.g., CSS and JavaScript) from the public folder
-// app.use(express.static('public'))
-
-// Serve static assets (e.g., CSS and JavaScript) from the public folder
-app.use(express.static(path.join(__dirname, "backend", "FrontEnd", "public")));
-
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
 const routerAdmin = require("./Routes/adminrouter");
 const routerBanker = require("./Routes/bankerrouter");
 const routerRM = require("./Routes/rmrouter");
 
-// Set EJS as the view engine
-// app.set("view engine", "ejs")
-
-
 app.use("/admin", routerAdmin);
 app.use("/banker", routerBanker);
 app.use("/rm", routerRM);
 
-//The 404 Route (ALWAYS Keep this as the last route)
+// The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function (req, res) {
   res.render('404/404');
 });
@@ -50,6 +42,6 @@ app.listen(PORT, "0.0.0.0", () => {
 //   cert: fs.readFileSync(path.join(__dirname,'cert', 'cert.pem'))
 // }, app)
 
-// sslServer.listen(PORT, ()=>{
+// sslServer.listen(PORT, () => {
 //   console.log(`Server started on port ${PORT}`);
 // })

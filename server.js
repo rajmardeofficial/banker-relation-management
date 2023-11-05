d94107d75de4e5f802f460cea32d040cbf0f3496
Raj Marde <rajmarde615@gmail.com>
2023-11-05 23:34:03 +0530

    jwt cooke changes done

--- server.js
+++ server.js
@@ -33,15 +33,15 @@ app.get('*', function (req, res) {
   res.render('404/404');
 });
 
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
-
-const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname,'cert', 'cert.pem'))
-}, app)
-
-sslServer.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
-})
+});
+
+// const sslServer = https.createServer({
+//   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname,'cert', 'cert.pem'))
+// }, app)
+
+// sslServer.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// })
